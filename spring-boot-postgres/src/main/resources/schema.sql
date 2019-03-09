abdd6428292f56f7d8715bc8680d02f6f6b94050
kcaiueo <kcwebapply@gmail.com>
2019-03-10 00:05:19 +0900

    add schema.sql

---
+++ spring-boot-postgres/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create table userdata (id integer,name text, score integer );
+insert into userdata values('1')
+
+create table post (postid integer, userid integer,content text );
+insert into post values('1','1','sampleテキスト','sample1’);
+insert into post values('2','1','sampleテキスト2','sample2');
+insert into post values('3','1','sampleテキスト3','sample3');
